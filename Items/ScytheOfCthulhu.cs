d2d7b3fdeb8ada541d486e647fa899bd4d82bf84
shirtydiamond33 <kyrixty@gmail.com>
2021-08-14 20:16:42 -0400

    add ScytheOfCthulhu item + projectile

---
+++ Items/ScytheOfCthulhu.cs
@@ -0,0 +1,41 @@
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace TestingShitOutMan.Items
+{
+	public class ScytheOfCthulhu : ModItem
+	{
+		public override void SetStaticDefaults() 
+		{
+			DisplayName.SetDefault("ScytheOfCthulhu"); // By default, capitalization in classnames will add spaces to the display name. You can customize the display name here by uncommenting this line.
+			Tooltip.SetDefault("Cthulhu's favourite weapon.");
+		}
+
+		public override void SetDefaults() 
+		{
+			item.damage = 50;
+			item.melee = true;
+			item.width = 40;
+			item.height = 40;
+			item.UseSound = SoundID.Item15; // mod.GetLegacySoundSlot(SoundType.Item, "Sounds/Item/LAFLAME");
+			item.useTime = 20;
+			item.useAnimation = 20;
+			item.useStyle = 1;
+			item.knockBack = 6;
+			item.value = 10000;
+			item.rare = 10;
+			item.autoReuse = true;
+			item.shoot = mod.ProjectileType("ScytheOfCthulhuProjectile");
+			item.shootSpeed = 5f;
+		}
+
+		public override void AddRecipes() 
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.DirtBlock, 10);
+			recipe.AddTile(TileID.WorkBenches);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+	}
+}
---
+++ Projectiles/ScytheOfCthulhuProjectile.cs
@@ -0,0 +1,70 @@
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace TestingShitOutMan.Projectiles
+{
+    public class ScytheOfCthulhuProjectile : ModProjectile
+    {
+        private float rotationScale = 0.2f / 100; // Sets speed of rotation of projectile
+        private float velocityScale = 0.98f; // Loses 2% of its value each frame when multiplied by this.
+        private int fadeScale; // Defined in SetDefaults, states how fast the projectile fades (lower value = fade sooner).
+        public override void SetDefaults()
+        {
+            projectile.width = 12;
+            projectile.height = 50;
+            projectile.penetrate = 30;
+            projectile.friendly = true;
+            projectile.melee = true;
+            projectile.tileCollide = false;
+            projectile.light = 0.4f;
+            projectile.timeLeft = 200; // frames
+            fadeScale = projectile.timeLeft*15;
+            projectile.ignoreWater = true;
+            projectile.extraUpdates = 1;
+            projectile.rotation = (float)Math.Atan2((double)projectile.velocity.Y, (double)projectile.velocity.X) + 1.57f;
+            //projectile.type = 274; // Sets projectile to death sickle projectiles.
+        }
+
+        public override void AI()
+        {
+            // Dust trail
+            Dust.NewDust(projectile.position + projectile.velocity, projectile.width, projectile.height, 21, projectile.velocity.X * -0.5f, projectile.velocity.Y * -0.5f); // Create dust trail following projectile (15 is the dust, change for a different type)
+            
+            // Lighting (purple is 1 part red, 0 parts green, 1 part blue (1:0:1))
+            Lighting.AddLight(projectile.position, 1f, 0f, 1f);
+
+            // Speed (slowing down the closer we get to the end).
+            projectile.velocity.X *= velocityScale;
+            projectile.velocity.Y *= velocityScale;
+
+            // Projectile Spin
+            if (projectile.velocity.X < 0) {  // Moving left, invert direction of sprite and direction of its rotation
+                projectile.spriteDirection = -1; // Invert direction of sprite (flip the sprite), which also inverts its rotation (sprite now faces and spins left instead of right.)
+                //Main.NewText(projectile.rotation.ToString() + " " + projectile.spriteDirection.ToString(), 0, 0, 255, true);
+            } 
+            projectile.rotation += (projectile.spriteDirection * projectile.timeLeft * rotationScale);
+
+            // Projectile fade
+            projectile.alpha = fadeScale / projectile.timeLeft; 
+        }
+
+        public override void PostAI() // called after each AI() call
+        {
+            // maybe some velocity checks?
+        }
+
+        public override void Kill(int timeLeft) // Called on projectile kill (obviously)
+        {
+            // Create explosion
+            // Main.PlaySound(SoundID.Item14, projectile.position);
+            // Create dust
+            // for (int i=0; i<10; i++) {
+            //     Dust.NewDust(projectile.position + projectile.velocity, projectile.width, projectile.height, 107, projectile.velocity.X * -0.5f, projectile.velocity.Y * -0.5f);
+            // }
+        }
+    }
+}
